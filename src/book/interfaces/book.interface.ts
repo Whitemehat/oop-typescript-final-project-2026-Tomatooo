a176ec908e37c861b969e67e0bc95cdcb938a56f
Whitemehat <wong1234.pe@gmail.com>
2026-02-14 06:32:40 +0700

    fix interface to use enum

--- src/book/interfaces/book.interface.ts
+++ src/book/interfaces/book.interface.ts
@@ -1,5 +1,9 @@
+import { BookCategory } from "../enums/book-catagories.enums";
+
 export interface Book {
     id: number;
     name: string;
     author: string;
+    category: BookCategory;
+    language: string;
 }
