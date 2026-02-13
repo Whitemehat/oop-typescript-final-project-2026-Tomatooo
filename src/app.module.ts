8899d4853faf582fcda28066dac5f58491a669b1
Whitemehat <wong1234.pe@gmail.com>
2026-02-14 02:43:23 +0700

    create book resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BookController } from './book/book.controller';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [BookModule],
+  controllers: [BookController],
   providers: [],
 })
 export class AppModule {}
